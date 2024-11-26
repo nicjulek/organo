8c1aef64c54db0be428a92ed4e72301077dd90d4
Nicole Julek Klazura <143097595+nicjulek@users.noreply.github.com>
2024-11-26 12:09:27 -0300

    Aula 3 - Usando useState
    Aula 3 - Usando useState

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -1,22 +1,28 @@
+import { useState } from 'react'
 import './CampoTexto.css'
 
 const CampoTexto = (props) => {
 
     const placeholderModificada = `${props.placeholder}...` 
 
-    let valor = 'Guilherme Silveira'
+    //let valor = 'Nicole'
+
+    const[valor, setValor] = useState('Nicole') /*seria tipo um 'let valor = ""' orém o useState prepara 2 obetos,
+    uma para vc ler e outro pra vc escrever (setter). Nisso ele participa assim do ciclo de vida dele.
+    Conforme esse valor muda, esse estado muda junto, assim o react sabe que ele precisa renderiza*/ 
 
     const aoDigitado = (evento) => {
-        valor = evento.target.value
+        props.aoAlterado(evento.target.value)
         console.log(valor)
     }
+
 
     return (
         <div className="campo-texto">
             <label>
                 {props.label}
             </label>
-            <input value={valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={placeholderModificada}/>
+            <input value={props.valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={placeholderModificada}/>
         </div>
     )
 }

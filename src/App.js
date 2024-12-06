768b655a5a5292be461d269e72e87857ec24561a
Nicole Julek Klazura <143097595+nicjulek@users.noreply.github.com>
2024-12-06 12:16:59 -0300

    Aula 04 - Criação de lista de times
    Criação de lista de times

--- src/App.js
+++ src/App.js
@@ -1,8 +1,47 @@
 import { useState } from 'react';
 import Banner from './componentes/Banner';
 import Formulario from './componentes/Formulario';
+import Time from './componentes/Time';
 
 function App() {
+
+  const times = [
+    {
+      nome: 'Programação',
+      corPrimaria: '#57c278',
+      corSecundaria: '#d9f7e9'
+    },
+    {
+      nome: 'Front-End',
+      corPrimaria: '#82cffa',
+      corSecundaria: '#e8f8ff'
+    },
+    {
+      nome: 'Data Science',
+      corPrimaria: '#a6d157',
+      corSecundaria: '#f0f8e2'
+    },
+    {
+      nome: 'Devops',
+      corPrimaria: '#e06b69',
+      corSecundaria: '#fde7e8'
+    },
+    {
+      nome: 'UX e Design',
+      corPrimaria: '#d8',
+      corSecundaria: ''
+    },
+    {
+      nome: 'Mobile',
+      corPrimaria: '',
+      corSecundaria: ''
+    },
+    {
+      nome: 'Inovação e Gestão',
+      corPrimaria: '',
+      corSecundaria: ''
+    }
+  ]
 
   const [colaboradores, setColaboradores] = useState([])
 
@@ -14,7 +53,10 @@ function App() {
   return (
     <div className="App">
       <Banner />
-      <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
+      <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)} />
+      <Time nome="Programação" />
+      <Time nome="Front-end" />
+      <Time nome="Data Science" />
     </div>
   );
 }
